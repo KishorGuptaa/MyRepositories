00cd8a43b1987946be3195b709a925c536d9a4d7
ASUS <ASUS@LAPTOP-L2CO2QF6>
2022-07-18 12:54:20 +0530

    [Kishor] : Demo of GitHub Repositiry

---
+++ src/demo/sample.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class sample {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+
+
+	}
+
+}
